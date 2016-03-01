8373d9c820214ba309d36e539b5ed9644e5b3593
SampathGK <gksampath@outlook.com>
2016-03-01 13:56:24 +0530

    Update nnCostFunction.m
    added a comment to notify mistakes used in representation of terminologies.

--- SampthCourse/machine-learning-ex4/ex4/nnCostFunction.m
+++ SampthCourse/machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,7 +62,7 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
+% NOTE :  I have interchanged the term used to notate Z and A as in Z = sigmoid(A)
 
 % FeedForward : Forward Calculation Propogation for finding the h(theta)
 % the Exercise has one hidden layer 
